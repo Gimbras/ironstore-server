f8575fc5a2fd5d73099b8c41c9bcaca2e2f1aa43
cullenwk <92733382+cullenwk@users.noreply.github.com>
2021-12-09 16:17:21 +0100

    first commit

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ require("./config")(app);
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
-app.use("/api", allRoutes);
+app.use(allRoutes);
+
+const orderRoutes = require("./routes/Order.route");
+app.use(orderRoutes);
+
+const productsRoutes = require("./routes/Products.route");
+app.use(productsRoutes);
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
